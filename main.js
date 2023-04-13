a25d5ce7cee69f96bbc504d0908a33a144ca61d5
a0716116z <65119444+a0716116z@users.noreply.github.com>
2023-04-14 00:46:20 +0800

    initize

---
+++ main.js
@@ -0,0 +1,60 @@
+window.onload = function(){
+  callwapp();
+  processUser()
+
+}
+let send=false;
+function processUser()
+{
+  let parameters = location.search.substring(1).split("&");
+
+  let temp = parameters[0].split("=");
+  user = unescape(temp[1]);
+  temp = parameters[1].split("=");
+  volume = unescape(temp[1]);
+  document.getElementById("user").innerHTML = user;
+  document.getElementById("volume").innerHTML = volume+' ml';
+  if(send==false){
+     send_data(user, volume);
+  }
+  // setInterval(auto_close, 1500);
+}
+function auto_close(){
+  window.open('', '_self', '');
+  window.close();
+}
+
+function send_data(user, volume) {
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbyUWgNSHFRDs-Tl_m4CLugo2ghklyZryNCBV7VmgVad23dFYRA0TrUBTYTLDW55edtdHQ/exec",
+    data: {
+        "user": user,
+        "volume": volume,
+    },
+    success: function(response) {
+      if(response == "成功"){
+        alert("成功");
+        send=true;
+      }
+    },
+  });
+};
+function callwapp(){
+  $(".waterwave-canvas").waterwave({
+
+  // parent container
+  parent : '',
+
+  // color of waves
+  color : '#55adcf',
+
+  // or 'down'
+  direction: 'up',
+
+  // background color
+  background: ''
+  
+});
+
+}
+
